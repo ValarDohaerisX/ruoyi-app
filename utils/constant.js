a32713c1850cf5170b9e932ef182f54b811de97f
lzx <972619248@qq.com>
2022-08-29 13:03:16 +0800

    init ruoyi-app

---
+++ utils/constant.js
@@ -0,0 +1,9 @@
+const constant = {
+   avatar: 'vuex_avatar',
+   name: 'vuex_name',
+   nickName: 'vuex_nickName',
+   roles: 'vuex_roles',
+   permissions: 'vuex_permissions'
+ }
+
+ export default constant
